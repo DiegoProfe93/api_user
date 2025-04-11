e3a0d7e2713017c4aa2345046fd0aa7528b6def7
Diego Ovalle <you@example.com>
2025-04-11 01:53:20 -0400

    prueba allowedorigins

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+
 const allowedOrigins = [
-    "http://localhost:5173",
-    "https://incomparable-travesseiro-7e8ae9.netlify.app",
-    "https://otraprue.netlify.app"
-  ];
+  "http://localhost:5173",
+  "https://incomparable-travesseiro-7e8ae9.netlify.app",
+  "https://otraprue.netlify.app"
+];
 
 const app = express();
 
 app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("No permitido por CORS"));
-      }
-    },
-  }));
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
